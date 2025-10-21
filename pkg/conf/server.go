94dafb753310c59ff037c068500f0b7e0f1b2906
wujunjie <wujunjie@zdns.cn>
2025-10-21 08:44:47 +0800

    fix: 修改文件

--- pkg/conf/server.go
+++ pkg/conf/server.go
@@ -48,21 +48,6 @@ type Discovery struct {
 	DeregisterTime int `yaml:"deregister_time"`
 }
 
-type Log struct {
-	Label             string `yaml:"label" env:"LOG_LABEL"`
-	LogFileName       string `yaml:"file_name" env:"LOG_FILE_NAME"`
-	LogDirPath        string `yaml:"dir_path" env:"LOG_DIR_PATH"`
-	LogLevel          string `yaml:"log_level" env:"LOG_LEVEL"`
-	MaxSize           int    `yaml:"max_size" env:"LOG_MAX_SIZE"`
-	MaxBackups        int    `yaml:"max_backups" env:"LOG_MAX_BACKUPS"`
-	Compress          bool   `yaml:"log_compress" env:"LOG_COMPRESS"`
-	DisableHTMLEscape bool   `yaml:"disable_html_escape" env:"LOG_DISABLE_HTML_ESCAPE"`
-	DisableQuote      bool   `yaml:"disable_quote" env:"LOG_DISABLE_QUOTE"`
-
-	ToStdout bool `yaml:"to_stdout" env:"LOG_TO_STDOUT"`
-	IsJson   bool `yaml:"is_json" env:"LOG_IS_JSON"`
-}
-
 type I18N struct {
 	// 可支持语言(en/zh)
 	Langs []string `yaml:"langs"`
